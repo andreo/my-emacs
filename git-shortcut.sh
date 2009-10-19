44bb57fc8702b8997b3cfd0cb28d2a20d5e5dc02
andreo <andreo@giga-mega.(none)>
2009-10-19 22:52:42 +0300

    added git-shortucts

---
+++ git-shortcut.sh
@@ -0,0 +1,10 @@
+sudo git config --global alias.cf config
+sudo git cf --global alias.cfg 'config --global'
+sudo git cfg color.ui auto
+sudo git cfg alias.st status
+sudo git cfg alias.lg 'log --pretty=oneline --abbrev=6'
+sudo git cfg alias.cm commit
+sudo git cfg alias.cmm 'commit -m'
+sudo git cfg alias.co 'checkout'
+sudo git cfg alias.coh 'checkout HEAD'
+sudo git cfg alias.br 'brunch'
