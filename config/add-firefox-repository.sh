e77d79c25de00f92c89044537619fd075e663ccd
tetiana <tetiana@tetiana-laptop.(none)>
2010-02-16 10:11:29 +0200

    add package signing key for mozilla

--- config/add-firefox-repository.sh
+++ config/add-firefox-repository.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 echo -e "\ndeb http://downloads.sourceforge.net/project/ubuntuzilla/mozilla/apt all main" | sudo tee -a /etc/apt/sources.list > /dev/null
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com C1289A29
