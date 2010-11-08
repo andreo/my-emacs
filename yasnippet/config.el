011ff95d5328d31976770620f63d3bacd90c2422
Andreo <andrey.torba@gmail.com>
2010-11-08 22:38:42 +0200

    delete compile-bundle.el file

--- yasnippet/config.el
+++ yasnippet/config.el
@@ -4,9 +4,23 @@
 
 (add-to-list 'load-path my/yas-directory)
 
+(defun my/yas/compile-bundle ()
+  "Compile yasnippet bundle with my local settings"
+  (interactive)
+  (let* ((default-directory my/yas-directory))
+    (add-to-list 'load-path (concat default-directory "yasnippet/"))
+    (require 'yasnippet)
+
+    (yas/compile-bundle "yasnippet/yasnippet.el"
+                        "yasnippet-bundle.el"
+                        "snippets/"
+                        nil
+                        "yasnippet/dropdown-list.el")
+    (byte-compile-file "yasnippet-bundle.el")))
+
 (require 'yasnippet-bundle)
-(load-file (concat my/yas-directory "compile-bundle.el"))
 
 (setq yas/triggers-in-field t)
 (setq yas/also-auto-indent-first-line t)
 (setq yas/root-directory (concat my/yas-directory "snippets/"))
+
